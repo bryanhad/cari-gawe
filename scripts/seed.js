11c9c0cb428029921e0a04ff1c5832d557d003f3
Bryan Hadinata <bryanhadinata76@gmail.com>
2024-04-02 01:32:51 +0700

    feat: add seed script

---
+++ scripts/seed.js
@@ -0,0 +1,29 @@
+const { placeholderJobs } = require("./placeholder-data");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await Promise.all(
+    placeholderJobs.map(async (job) => {
+      await prisma.job.upsert({
+        where: {
+          slug: job.slug,
+        },
+        update: job,
+        create: job,
+      });
+    }),
+  );
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+    console.log("Successfully seed the database!");
+  })
+  .catch(async (err) => {
+    console.error("Error while seeding database", err);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
