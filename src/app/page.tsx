0332871f1e880e6a653875ba3b68a2bace1a6c9b
Bryan Hadinata <bryanhadinata76@gmail.com>
2024-04-02 04:26:43 +0700

    feat: add filtering to job results! in this filtering feature, we use the 'full-text' search feature in postgres! this will allow us to query all entries that contains a certain text in any String fields on our model! pretty neat.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,38 @@
 import JobFilterSidebar from "@/components/JobFilterSidebar";
-import JobListItem from "@/components/JobListItem";
-import db from "@/lib/db";
+import JobResults from "@/components/JobResults";
+import H1 from "@/components/ui/h1";
+import { JobFilterValues } from "@/lib/validation";
 
-async function HomePage() {
-    const jobs = await db.job.findMany({
-        where: { approved: true },
-    });
+// to get the search params of a page we can access it from the searchParams prop that is available to every server component page in nextjs
+type HomePageProps = {
+    searchParams: {
+        query?: string;
+        type?: string;
+        location?: string;
+        remote?: string;
+    };
+};
+
+function HomePage({
+    searchParams: { query, location, type, remote },
+}: HomePageProps) {
+    const filterValues: JobFilterValues = {
+        query,
+        type,
+        location,
+        // we convert the remote into a boolean by using a deep equal operator since the remote query only can be either "true" or undefined
+        remote: remote === "true",
+    };
+
     return (
         <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
             <div className="space-y-5 text-center">
-                <h1 className="text-4xl font-bold tracking-tight lg:text-5xl">Developer Jobs</h1>
+                <H1>Developer Jobs</H1>
                 <p className="text-muted-foreground">Find your dream job.</p>
             </div>
-            <section className="flex flex-col md:flex-row gap-4">
-              <JobFilterSidebar/>
-                <div className="space-y-4 grow">
-                    {jobs.map((job) => (
-                        <JobListItem job={job} key={job.id} />
-                    ))}
-                </div>
+            <section className="flex flex-col gap-4 md:flex-row">
+                <JobFilterSidebar />
+                <JobResults filterValues={filterValues}/>
             </section>
         </main>
     );
---
+++ src/components/JobResults.tsx
@@ -0,0 +1,60 @@
+import db from "@/lib/db";
+import React from "react";
+import JobListItem from "./JobListItem";
+import { JobFilterValues } from "@/lib/validation";
+import { Prisma } from "@prisma/client";
+
+type JobResultsProps = {
+    filterValues: JobFilterValues;
+};
+
+async function JobResults({
+    filterValues: { query, location, remote, type },
+}: JobResultsProps) {
+    const searchString = query
+        ?.split(" ")
+        .filter((word) => word.length > 0)
+        .join(" & ");
+
+    // The code above will do something like...
+    // 1. query input: "nextjs frontend" => "nextjs & frontend"
+    // 2. query input: "microsoft     fullstack" => "nextjs & frontend"
+
+    const searchFilter: Prisma.JobWhereInput = searchString
+        ? {
+              // we use "OR" filter, so that the searh filter will work on any columns that we specify
+              OR: [
+                  { title: { search: searchString } },
+                  { companyName: { search: searchString } },
+                  { type: { search: searchString } },
+                  { locationType: { search: searchString } },
+                  { location: { search: searchString } },
+              ],
+          }
+        : {};
+
+    const whereFilter: Prisma.JobWhereInput = {
+        AND: [
+            searchFilter,
+            type ? { type } : {},
+            location ? { location } : {},
+            remote ? { locationType: "Remote" } : {},
+            { approved: true },
+        ],
+    };
+
+    const jobs = await db.job.findMany({
+        where: whereFilter,
+        orderBy: { createdAt: "desc" },
+    });
+
+    return (
+        <div className="grow space-y-4">
+            {jobs.map((job) => (
+                <JobListItem job={job} key={job.id} />
+            ))}
+        </div>
+    );
+}
+
+export default JobResults;
