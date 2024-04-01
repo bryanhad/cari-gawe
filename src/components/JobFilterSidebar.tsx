0cec2af90175fef17d9d2821b9c7ce89b3392935
Bryan Hadinata <bryanhadinata76@gmail.com>
2024-04-02 02:30:06 +0700

    feat: add job list item

---
+++ src/components/JobFilterSidebar.tsx
@@ -0,0 +1,10 @@
+function JobFilterSidebar() {
+    // h-fit is to set the height of the parent as high as the content! nice!
+    return (
+        <aside className="sticky top-0 h-fit rounded-lg border bg-background md:w-[260px]">
+            <form action="">filter Sidebar</form>
+        </aside>
+    );
+}
+
+export default JobFilterSidebar;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,20 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { formatDistanceToNowStrict } from "date-fns";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+export function formatToMoney(amount: number): string {
+    return new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+    }).format(amount);
+}
+
+export function getRelativeDate(from: Date) {
+    return formatDistanceToNowStrict(from, {
+        addSuffix: true,
+    });
 }
